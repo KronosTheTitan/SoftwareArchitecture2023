fc4710a8a55eef5b04f91166d95f30054e247715
KronosTheTitan <tjalle@borgersonline.nl>
2023-12-24 17:01:20 +0100

    getting ready for computer OS change

---
+++ Assets/Scripts/MainHud.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainHud : MonoBehaviour
+{
+    [SerializeField] private Slider playerHealthBarOuter;
+    [SerializeField] private Slider playerHealthBarInner;
+    [SerializeField] private Slider buildTimeRemaining;
+
+    [SerializeField] private TMP_Text waveNumber;
+    [SerializeField] private TMP_Text moneyNumber;
+
+    private void Start()
+    {
+        EventBus.OnPlayerTakeDamage += UpdatePlayerHealthBar;
+    }
+    
+    private void Update()
+    {
+        playerHealthBarInner.value = Mathf.Lerp(playerHealthBarInner.value, playerHealthBarOuter.value, Time.time);
+    }
+
+    private void UpdatePlayerHealthBar(int newHealth, int damageTaken)
+    {
+        playerHealthBarOuter.value = newHealth;
+    }
+}
